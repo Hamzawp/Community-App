52bb4fb1afa466c043932d2a1f24be35261791ed
Hamza Sayyed <hamzawp404@gmail.com>
2023-01-22 12:10:28 +0530

    Proj completed

---
+++ client/src/App.js
@@ -0,0 +1,74 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Chat from "./pages/Chat";
+import { useSelector } from "react-redux";
+import { useState,useEffect } from "react";
+import { AppContext, socket } from "./context/appContext";
+import PropagateLoader from "react-spinners/PropagateLoader";
+
+
+const override= {
+    display: "flex",
+    justifyContent: "center",
+    alignItems:"center",
+    // margin: "40vh auto",
+    borderColor: "red",
+    height:"60vh"
+  };
+  
+
+
+function App() {
+    const [rooms, setRooms] = useState([]);
+    const [currentRoom, setCurrentRoom] = useState([]);
+    const [members, setMembers] = useState([]);
+    const [messages, setMessages] = useState([]);
+    const [privateMemberMsg, setPrivateMemberMsg] = useState({});
+    const [newMessages, setNewMessages] = useState({});
+    const user = useSelector((state) => state.user);
+
+    const [loading, setloading] = useState(false);
+
+    useEffect(() => {
+      setloading(true)
+     setTimeout(()=>{
+      setloading(false)
+     },3000)
+    }, []);
+    return (
+            <div className="temp">
+        <AppContext.Provider value={{ socket, currentRoom, setCurrentRoom, members, setMembers, messages, setMessages, privateMemberMsg, setPrivateMemberMsg, rooms, setRooms, newMessages, setNewMessages }}>
+        {loading  ?  
+    <PropagateLoader
+    color={"#ffffff"}
+    loading={loading}
+    cssOverride={override}
+    size={45}
+    aria-label="Loading Spinner"
+    data-testid="loader"
+  />:
+            <BrowserRouter>
+                <Navigation />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    {!user && (
+                        <>
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/signup" element={<Signup />} />
+                        </>
+                    )}
+                    <Route path="/chat" element={<Chat />} />
+                </Routes>
+            </BrowserRouter>
+}
+        </AppContext.Provider>
+        </div>
+    );
+}
+
+export default App;
---
+++ client/src/components/Navigation.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { Button, Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { LinkContainer } from "react-router-bootstrap";
+import logo from "../assets/logoca.png";
+import { useLogoutUserMutation } from "../services/appApi";
+
+function Navigation() {
+  const user = useSelector((state) => state.user);
+  const [logoutUser] = useLogoutUserMutation();
+  async function handleLogout(e) {
+    e.preventDefault();
+    await logoutUser(user);
+    // redirect to home page
+    window.location.replace("/");
+  }
+  const PF = user ? `http://localhost:3001/${user.name}` : `/`;
+  return (
+    // <Navbar bg="dark" varient="dark">
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <LinkContainer to="/">
+          <Navbar.Brand>
+            <img src={logo} style={{ width: "180px", height: "50px", objectFit: "cover" }} alt="logo" />
+          </Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            {!user && (
+              <LinkContainer to="/login">
+                <Nav.Link>Login</Nav.Link>
+              </LinkContainer>
+            )}
+            <LinkContainer to="/chat">
+              <Nav.Link>Chat</Nav.Link>
+            </LinkContainer>
+            {user && (
+              <NavDropdown
+                title={
+                  <>
+                    <img
+                      src={user.picture}
+                      style={{
+                        width: 30,
+                        height: 30,
+                        marginRight: 10,
+                        objectFit: "cover",
+                        borderRadius: "50%",
+                      }}
+                    />
+                    {user.name}
+                  </>
+                }
+                id="basic-nav-dropdown"
+              >
+                <NavDropdown.Item href={PF}>
+                  Video Chat
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Saved Messages
+                </NavDropdown.Item>
+
+                <NavDropdown.Item>
+                  <Button variant="danger" onClick={handleLogout}>
+                    Logout
+                  </Button>
+                </NavDropdown.Item>
+              </NavDropdown>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navigation;
